39bdb119c1515c662f47dd302a88d591bdb7d9d8
Piton <darnon88@gmail.com>
2021-05-27 12:52:51 +0300

    some

--- client/App.tsx
+++ client/App.tsx
@@ -1,40 +1,13 @@
 import React from 'react';
-import {} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import LoginScreen from './screens/auth/LoginScreen';
-import RegisterScreen from './screens/auth/RegisterScreen';
-import PostsScreen from './screens/main/PostsScreen';
-import CreateScreen from './screens/main/CreateScreen';
-import ProfileScreen from './screens/main/ProfileScreen';
-
-const AuthStack = createStackNavigator();
-const MainTab = createBottomTabNavigator();
-
-const useRoute = (isAuth) => {
-  if(!isAuth ) {
-   return <AuthStack.Navigator>
-      <AuthStack.Screen options={{headerShown: false}} name='Login' component={LoginScreen}/>
-      <AuthStack.Screen options={{headerShown: false}} name='Register' component={RegisterScreen}/>
-    </AuthStack.Navigator> 
-  };
-  return <MainTab.Navigator>
-    <MainTab.Screen name='Posts' component={PostsScreen} />
-    <MainTab.Screen name='Create' component={CreateScreen} />
-    <MainTab.Screen name='Profile' component={ProfileScreen} />
-  </MainTab.Navigator>
-};
+import useRoute from './src/router'
 
 export default function App() {
-  const routing = useRoute(false);
+  const routing = useRoute(true);
   return (
     <NavigationContainer>
       {routing}
     </NavigationContainer> 
   );
 };
-
-
-// auth
 
---
+++ client/src/router.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import LoginScreen from './screens/Auth/LoginScreen';
+import RegisterScreen from './screens/Auth/RegisterScreen';
+import PostsScreen from './screens/main/PostsScreen';
+import CreateScreen from './screens/main/CreateScreen';
+import ProfileScreen from './screens/main/ProfileScreen';
+
+import BurgerMenu from './assets/image/Group 1710.svg';
+
+const AuthStack = createStackNavigator();
+const MainTab = createBottomTabNavigator();
+
+const useRoute = (isAuth) => {
+  if(!isAuth ) {
+   return <AuthStack.Navigator>
+      <AuthStack.Screen options={{headerShown: false}} name='Login' component={LoginScreen}/>
+      <AuthStack.Screen options={{headerShown: false}} name='Register' component={RegisterScreen}/>
+    </AuthStack.Navigator> 
+  };
+  return <MainTab.Navigator tabBarOptions={{showLabel: false}}>
+    <MainTab.Screen options={{
+        tabBarIcon: ({focused, size, color}) => {
+            <BurgerMenu style={{width: 10, height: 10}} />
+        },
+    }} name='Posts' component={PostsScreen} />
+    <MainTab.Screen name='Create' component={CreateScreen} />
+    <MainTab.Screen name='Profile' component={ProfileScreen} />
+  </MainTab.Navigator>
+};
+
+export default useRoute;
