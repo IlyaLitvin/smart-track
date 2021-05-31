486a938aa17d19cd8469711d8e7999ec2790e79a
Piton <darnon88@gmail.com>
2021-05-31 11:03:32 +0300

    smth

--- client/src/screens/Staff/Staff.tsx
+++ client/src/screens/Staff/Staff.tsx
@@ -1,14 +1,73 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet, ScrollView} from 'react-native';
 import Header from '../../components/Header/Header';
+import {DrawerNavigationProp} from '@react-navigation/drawer';
+import {Button} from '../../common/button/Button';
 
-export default function Staff({navigation}) {
+type HeaderProps = {
+  navigation: DrawerNavigationProp<any, any>;
+};
+
+export default function Staff({navigation}: HeaderProps) {
   return (
     <>
       <Header navigation={navigation} />
-      <View>
-        <Text>Staff</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <View style={styles.docWrapper}>
+          <View>
+            <Text style={styles.postNumber}>1</Text>
+          </View>
+          <View style={styles.mainBox}>
+            <Text style={styles.name}>Alex Sample</Text>
+            <Text style={styles.mail}>frontdesk@gmail.com</Text>
+            <Text style={styles.phone}>0959423146</Text>
+            <View>
+              <Text>Rooms</Text>
+              <Text>1a, 1b, 3f</Text>
+            </View>
+            <View>
+              <View></View>
+              <View></View>
+              <View></View>
+              <View></View>
+              <View></View>
+              <View></View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </>
   );
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#F6F6FF",
+      paddingLeft: 16,
+      paddingRight: 16,
+    },
+    docWrapper: {
+      backgroundColor: "#FFF",
+      width: 327,
+      height: 203,
+      borderTopLeftRadius: 20,
+      borderBottomRightRadius: 20,
+      marginTop: 15,
+    },
+    postNumber: {
+
+    },
+    mainBox: {
+
+    },
+    name: {
+
+    },
+    mail: {
+
+    },
+    phone: {
+
+    },
+});
