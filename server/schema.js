dece27219ef2e5db391b6d4b2b6d2b1aedcd7592
Piton <darnon88@gmail.com>
2021-06-01 16:17:11 +0300

    graphql done

---
+++ server/schema.js
@@ -0,0 +1,100 @@
+const {makeExecutableSchema} = require('graphql-tools');
+const { resolvers } = require('./resolvers');
+const schema = makeExecutableSchema({
+    typeDefs:`    
+    type Doctor {
+        id: ID
+        name: String!
+        specialization: String!
+        email: String!
+        phone: String!
+        rooms: [Room]
+    }
+    type Room {
+        id: ID
+        name: String!
+        timeStatus: String!
+        status: String!
+        assignedDoctorId: Int!
+    }
+    type Assistant {
+        id: ID
+        name: String!
+        email: String!
+        phone: String!
+    }
+    type Receptionist {
+        id: ID
+        name: String!
+        email: String!
+        phone: String!
+    }
+    type Alert {
+        id: ID
+        name: String
+        color: String
+        role: String
+    }
+
+    input DoctorInput {
+        id: ID
+        name: String!
+        profession: String!
+        email: String!
+        phone: String!
+    }
+    input RoomInput {
+        id: ID
+        name: String!
+        timeStatus: String!
+        status: String!
+        assignedDoctorId: Int!
+    }
+    input AssistantInput {
+        id: ID
+        name: String!
+        email: String!
+        phone: String!
+    }
+    input ReceptionistInput {
+        id: ID
+        name: String!
+        email: String!
+        phone: String!
+    }
+    input AlertInput {
+        id: ID
+        name: String
+        color: String
+        role: String
+    }
+
+    type Query {
+        getAllDoctors: [Doctor]
+        getDoctor(id: ID!): Doctor
+        getAllAssistants: [Assistant]
+        getAssistant(id: ID!): Assistant
+        getAllReceptionists: [Receptionist]
+        getReceptionist(id: ID!): Receptionist
+        getAllAlerts: [Alert]
+        getDoctorRooms(id: ID!): [Room]         
+    }
+    type Mutation {
+        createDoctor(doctor: DoctorInput): Doctor
+        deleteDoctor(doctorId: ID!): ID
+        updateDoctor(doctorId: ID!, doctorInput: DoctorInput!): Doctor        
+        createAssistant(assistant: AssistantInput): Assistant
+        deleteAssistant(assistantId: ID!): ID
+        updateAssistant(assistantId: ID!, assistantInput: AssistantInput!): Assistant
+        createReceptionist(receptionist: ReceptionistInput): Receptionist
+        deleteReceptionist(receptionistId: ID!): ID
+        updateReceptionist(receptionistId: ID!, receptionistInput: ReceptionistInput!): Receptionist
+        createAlert(alert: AlertInput): Alert
+        deleteAlert(alertId: ID!): ID
+        updateAlert(alertId: ID!, alertInput: AlertInput!): Alert
+    }
+`,
+resolvers
+})
+
+module.exports = schema
