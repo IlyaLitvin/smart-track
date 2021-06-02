f88029250e450a5fcadfe2cb9f5b5963e875dd61
AlexeyLocker <alexey.locker@gmail.com>
2021-06-02 17:43:44 +0300

    DragAndDrop

--- client/src/screens/Staff/Staff.tsx
+++ client/src/screens/Staff/Staff.tsx
@@ -21,15 +21,15 @@ export default function Staff({navigation}: HeaderProps) {
       </ScrollView>
     </>
   );
-};
+}
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#F6F6FF",
-      paddingLeft: 16,
-      paddingRight: 16,
-      fontFamily: "Poppins",      
-      fontStyle: "normal",   
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#F6F6FF',
+    paddingLeft: 16,
+    paddingRight: 16,
+    fontFamily: 'Poppins',
+    fontStyle: 'normal',
+  },
 });
